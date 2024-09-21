156cb278a9a418787f463080feb43130adb163aa
KomalMavani <102403515+KomalMavani@users.noreply.github.com>
2024-09-21 11:42:27 -0400

    request commit

--- index.js
+++ index.js
@@ -4,6 +4,30 @@ const app = express();  //create object of express
 app.use(express.json()); 
 const port = 3000; // Assign port on which want to run application
 
+let datastore = {
+    user: [{
+        "name":"Test1",
+        "age":25
+    },
+    {
+        "name":"Test2",
+        "age":26
+    },
+    {
+        "name":"Test3",
+        "age":29
+    }]
+}
+
+app.get('/getusers',(req, res) => {
+    res.json(datastore.user);
+})
+
+app.get('/postusers',(req, res) => {
+    const {name, age} = req.body;
+    res.json(datastore.user);
+})
+
 app.get('/hello',( req, res ) => {
     res.json({"key": "Komal"});
 })
